me-safty/dxcode

add configurable worktree location settings

apps/server/src/server.test.ts
@@ -57,6 +57,10 @@ import {
 import { GitCore, type GitCoreShape } from "./git/Services/GitCore.ts";
 import { GitManager, type GitManagerShape } from "./git/Services/GitManager.ts";
 import { GitStatusBroadcasterLive } from "./git/Layers/GitStatusBroadcaster.ts";
+import {
+  WorktreeLocationResolver,
+  type WorktreeLocationResolverShape,
+} from "./project/Services/WorktreeLocationResolver.ts";
 import { Keybindings, type KeybindingsShape } from "./keybindings.ts";
 import { Open, type OpenShape } from "./open.ts";
 import {
@@ -293,6 +297,7 @@ const buildAppUnderTest = (options?: {
     open?: Partial<OpenShape>;
     gitCore?: Partial<GitCoreShape>;
     gitManager?: Partial<GitManagerShape>;
+    worktreeLocationResolver?: Partial<WorktreeLocationResolverShape>;
     projectSetupScriptRunner?: Partial<ProjectSetupScriptRunnerShape>;
     terminalManager?: Partial<TerminalManagerShape>;
     orchestrationEngine?: Partial<OrchestrationEngineShape>;
@@ -383,6 +388,13 @@ const buildAppUnderTest = (options?: {
       Layer.provide(
         Layer.mock(GitCore)({
           ...options?.layers?.gitCore,
+        }),
+      ),
+      Layer.provide(
+        Layer.mock(WorktreeLocationResolver)({
+          resolveCreateWorktreePath: (input) =>
+            Effect.succeed(`/tmp/worktrees/${input.name.replace(/\//g, "-")}`),
+          ...options?.layers?.worktreeLocationResolver,
         }),
       ),
       Layer.provide(gitManagerLayer),
@@ -2997,7 +3009,7 @@ it.layer(NodeServices.layer)("server router seam", (it) => {
           cwd: "/tmp/project",
           branch: "main",
           newBranch: "t3code/bootstrap-branch",
-          path: null,
+          path: "/tmp/worktrees/t3code-bootstrap-branch",
         });
         assert.deepEqual(runForThread.mock.calls[0]?.[0], {
           threadId: ThreadId.make("thread-bootstrap"),

apps/server/src/ws.ts
@@ -30,6 +30,7 @@ import { ServerConfig } from "./config";
 import { GitCore } from "./git/Services/GitCore";
 import { GitManager } from "./git/Services/GitManager";
 import { GitStatusBroadcaster } from "./git/Services/GitStatusBroadcaster";
+import { WorktreeLocationResolver } from "./project/Services/WorktreeLocationResolver";
 import { Keybindings } from "./keybindings";
 import { Open, resolveAvailableEditors } from "./open";
 import { normalizeDispatchCommand } from "./orchestration/Normalizer";
@@ -112,12 +113,31 @@ const makeWsRpcLayer = (currentSessionId: AuthSessionId) =>
       const open = yield* Open;
       const gitManager = yield* GitManager;
       const git = yield* GitCore;
+      const worktreeLocationResolver = yield* WorktreeLocationResolver;
       const gitStatusBroadcaster = yield* GitStatusBroadcaster;
       const terminalManager = yield* TerminalManager;
       const providerRegistry = yield* ProviderRegistry;
       const config = yield* ServerConfig;
       const lifecycleEvents = yield* ServerLifecycleEvents;
       const serverSettings = yield* ServerSettingsService;
+
+      const resolveCreateWorktreePath = Effect.fn("ws.resolveCreateWorktreePath")(
+        function* (input: {
+          cwd: string;
+          branch: string;
+          newBranch?: string;
+          path: string | null;
+        }) {
+          if (input.path !== null) {
+            return input.path;
+          }
+
+          return yield* worktreeLocationResolver.resolveCreateWorktreePath({
+            projectRoot: input.cwd,
+            name: input.newBranch ?? input.branch,
+          });
+        },
+      );
       const startup = yield* ServerRuntimeStartup;
       const workspaceEntries = yield* WorkspaceEntries;
       const workspaceFileSystem = yield* WorkspaceFileSystem;
@@ -376,11 +396,21 @@ const makeWsRpcLayer = (currentSessionId: AuthSessionId) =>
             }
 
             if (bootstrap?.prepareWorktree) {
+              const worktreePath = yield* resolveCreateWorktreePath({
+                cwd: bootstrap.prepareWorktree.projectCwd,
+                branch: bootstrap.prepareWorktree.baseBranch,
+                path: null,
+                ...(bootstrap.prepareWorktree.branch
+                  ? { newBranch: bootstrap.prepareWorktree.branch }
+                  : {}),
+              });
               const worktree = yield* git.createWorktree({
                 cwd: bootstrap.prepareWorktree.projectCwd,
                 branch: bootstrap.prepareWorktree.baseBranch,
-                newBranch: bootstrap.prepareWorktree.branch,
-                path: null,
+                path: worktreePath,
+                ...(bootstrap.prepareWorktree.branch
+                  ? { newBranch: bootstrap.prepareWorktree.branch }
+                  : {}),
               });
               targetWorktreePath = worktree.worktree.path;
               yield* orchestrationEngine.dispatch({
@@ -755,7 +785,22 @@ const makeWsRpcLayer = (currentSessionId: AuthSessionId) =>
         [WS_METHODS.gitCreateWorktree]: (input) =>
           observeRpcEffect(
             WS_METHODS.gitCreateWorktree,
-            git.createWorktree(input).pipe(Effect.tap(() => refreshGitStatus(input.cwd))),
+            Effect.gen(function* () {
+              const worktreePath = yield* resolveCreateWorktreePath({
+                cwd: input.cwd,
+                branch: input.branch,
+                path: input.path,
+                ...(input.newBranch ? { newBranch: input.newBranch } : {}),
+              });
+              return yield* git
+                .createWorktree({
+                  path: worktreePath,
+                  cwd: input.cwd,
+                  branch: input.branch,
+                  ...(input.newBranch ? { newBranch: input.newBranch } : {}),
+                })
+                .pipe(Effect.tap(() => refreshGitStatus(input.cwd)));
+            }),
             { "rpc.aggregate": "git" },
           ),
         [WS_METHODS.gitRemoveWorktree]: (input) =>

tarik02/t3code

apps/server/src/server.test.ts
@@ -109,6 +109,10 @@ import {
   type ProjectSetupScriptRunnerShape,
 } from "./project/Services/ProjectSetupScriptRunner.ts";
 import {
+  WorktreeLocationResolver,
+  type WorktreeLocationResolverShape,
+} from "./project/Services/WorktreeLocationResolver.ts";
+import {
   RepositoryIdentityResolver,
   type RepositoryIdentityResolverShape,
 } from "./project/Services/RepositoryIdentityResolver.ts";
@@ -353,6 +357,7 @@ const buildAppUnderTest = (options?: {
     sourceControlRepositoryService?: Partial<SourceControlRepositoryService.SourceControlRepositoryServiceShape>;
     reviewService?: Partial<ReviewService.ReviewServiceShape>;
     vcsStatusBroadcaster?: Partial<VcsStatusBroadcaster.VcsStatusBroadcasterShape>;
+    worktreeLocationResolver?: Partial<WorktreeLocationResolverShape>;
     projectSetupScriptRunner?: Partial<ProjectSetupScriptRunnerShape>;
     terminalManager?: Partial<TerminalManagerShape>;
     orchestrationEngine?: Partial<OrchestrationEngineShape>;
@@ -660,6 +665,13 @@ const buildAppUnderTest = (options?: {
         Layer.mock(ProjectSetupScriptRunner)({
           runForThread: () => Effect.succeed({ status: "no-script" as const }),
           ...options?.layers?.projectSetupScriptRunner,
+        }),
+      ),
+      Layer.provide(
+        Layer.mock(WorktreeLocationResolver)({
+          resolveCreateWorktreePath: (input) =>
+            Effect.succeed(path.join("/tmp/worktrees", input.name.replace(/\//g, "-"))),
+          ...options?.layers?.worktreeLocationResolver,
         }),
       ),
       Layer.provide(
@@ -6140,7 +6152,7 @@ it.layer(NodeServices.layer)("server router seam", (it) => {
           cwd: "/tmp/project",
           refName: "main",
           newRefName: "t3code/bootstrap-refName",
-          path: null,
+          path: "/tmp/worktrees/t3code-bootstrap-refName",
         });
         assert.deepEqual(runForThread.mock.calls[0]?.[0], {
           threadId: ThreadId.make("thread-bootstrap"),

apps/server/src/ws.ts
@@ -77,6 +77,7 @@ import { VcsStatusBroadcaster } from "./vcs/VcsStatusBroadcaster.ts";
 import { VcsProvisioningService } from "./vcs/VcsProvisioningService.ts";
 import { GitWorkflowService } from "./git/GitWorkflowService.ts";
 import { ReviewService } from "./review/ReviewService.ts";
+import { WorktreeLocationResolver } from "./project/Services/WorktreeLocationResolver.ts";
 import { ProjectSetupScriptRunner } from "./project/Services/ProjectSetupScriptRunner.ts";
 import { RepositoryIdentityResolver } from "./project/Services/RepositoryIdentityResolver.ts";
 import { ServerEnvironment } from "./environment/Services/ServerEnvironment.ts";
@@ -237,6 +238,7 @@ const makeWsRpcLayer = (currentSession: AuthenticatedSession) =>
       const externalLauncher = yield* ExternalLauncher.ExternalLauncher;
       const gitWorkflow = yield* GitWorkflowService;
       const review = yield* ReviewService;
+      const worktreeLocationResolver = yield* WorktreeLocationResolver;
       const vcsProvisioning = yield* VcsProvisioningService;
       const vcsStatusBroadcaster = yield* VcsStatusBroadcaster;
       const terminalManager = yield* TerminalManager;
@@ -245,6 +247,23 @@ const makeWsRpcLayer = (currentSession: AuthenticatedSession) =>
       const config = yield* ServerConfig;
       const lifecycleEvents = yield* ServerLifecycleEvents;
       const serverSettings = yield* ServerSettingsService;
+      const resolveCreateWorktreePath = Effect.fn("ws.resolveCreateWorktreePath")(
+        function* (input: {
+          cwd: string;
+          refName: string;
+          newRefName?: string | undefined;
+          path: string | null;
+        }) {
+          if (input.path !== null) {
+            return input.path;
+          }
+
+          return yield* worktreeLocationResolver.resolveCreateWorktreePath({
+            projectRoot: input.cwd,
+            name: input.newRefName ?? input.refName,
+          });
+        },
+      );
       const startup = yield* ServerRuntimeStartup;
       const workspaceEntries = yield* WorkspaceEntries;
       const workspaceFileSystem = yield* WorkspaceFileSystem;
@@ -668,11 +687,19 @@ const makeWsRpcLayer = (currentSession: AuthenticatedSession) =>
             }
 
             if (bootstrap?.prepareWorktree) {
+              const worktreePath = yield* resolveCreateWorktreePath({
+                cwd: bootstrap.prepareWorktree.projectCwd,
+                refName: bootstrap.prepareWorktree.baseBranch,
+                path: null,
+                ...(bootstrap.prepareWorktree.branch
+                  ? { newRefName: bootstrap.prepareWorktree.branch }
+                  : {}),
+              });
               const worktree = yield* gitWorkflow.createWorktree({
                 cwd: bootstrap.prepareWorktree.projectCwd,
                 refName: bootstrap.prepareWorktree.baseBranch,
                 newRefName: bootstrap.prepareWorktree.branch,
-                path: null,
+                path: worktreePath,
               });
               targetWorktreePath = worktree.worktree.path;
               yield* orchestrationEngine.dispatch({
@@ -1260,7 +1287,20 @@ const makeWsRpcLayer = (currentSession: AuthenticatedSession) =>
         [WS_METHODS.vcsCreateWorktree]: (input) =>
           observeRpcEffect(
             WS_METHODS.vcsCreateWorktree,
-            gitWorkflow.createWorktree(input).pipe(Effect.tap(() => refreshGitStatus(input.cwd))),
+            Effect.gen(function* () {
+              const worktreePath = yield* resolveCreateWorktreePath({
+                cwd: input.cwd,
+                refName: input.refName,
+                path: input.path,
+                ...(input.newRefName ? { newRefName: input.newRefName } : {}),
+              });
+              return yield* gitWorkflow
+                .createWorktree({
+                  ...input,
+                  path: worktreePath,
+                })
+                .pipe(Effect.tap(() => refreshGitStatus(input.cwd)));
+            }),
             { "rpc.aggregate": "vcs" },
           ),
         [WS_METHODS.vcsRemoveWorktree]: (input) =>